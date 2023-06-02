1f3aa2371fc2b33a89886892183ed9b2da6b08f4
Nastia <adruts@mail.ru>
2023-06-03 00:41:22 +0300

    https://github.com/anastyadru/Dictionary_C_2.git

--- Dictionary_C_2/Forecast.cs
+++ Dictionary_C_2/Forecast.cs
@@ -2,16 +2,34 @@ using System;
 
 namespace Dictionary_C_2
 {
+    /// <summary>
+    /// Класс, представляющий прогноз погоды.
+    /// </summary>
     public class Forecast
     {
+        /// <summary>
+        /// Дата прогноза.
+        /// </summary>
         public DateTime Date { get; set; }
         
+        /// <summary>
+        /// Температура воздуха.
+        /// </summary>
         public double Temp { get; set; }
         
+        /// <summary>
+        /// Ощущаемая температура.
+        /// </summary>
         public double FeelsLike { get; set; }
         
+        /// <summary>
+        /// Давление воздуха.
+        /// </summary>
         public double Pressure { get; set; }
         
+        /// <summary>
+        /// Влажность воздуха.
+        /// </summary>
         public double Humidity { get; set; }
     }
 }
