7e6f9eb08836aef569c3d56e7ad1b23fbfb8d433
Nastia <adruts@mail.ru>
2023-06-03 00:07:27 +0300

    https://github.com/anastyadru/Dictionary_C_2.git

---
+++ Dictionary_C_2/Forecast.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Dictionary_C_2
+{
+    public class Forecast
+    {
+        public DateTime Date { get; set; }
+        
+        public double Temp { get; set; }
+        
+        public double FeelsLike { get; set; }
+        
+        public double Pressure { get; set; }
+        
+        public double Humidity { get; set; }
+    }
+}
